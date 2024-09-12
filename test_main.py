bec6e17ecc9679cb79561669a78238ec986917f1
gvenado28 <jaimevenado.1216@gmail.com>
2024-09-12 11:10:48 -0600

    3ra actualización y corrección de variable de mensaje

--- test_main.py
+++ test_main.py
@@ -2,4 +2,4 @@
 from app.main import ci_cd
 
 def test_hello_world():
-    assert ci_cd() == "Verificando la integración y entrega continua de mi R2"
+    assert ci_cd() == "Verificando la integración y entrega continua de mi R2!"
